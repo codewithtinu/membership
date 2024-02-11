57c9aa78185a15e220c74b8561e0e3051dc848c0
KuldeepSonune45 <famlight@gmail.com>
2024-02-11 13:17:23 +0530

    Updated  profile

--- client/src/pages/admin/UserProfile.js
+++ client/src/pages/admin/UserProfile.js
@@ -1,47 +1,47 @@
 const Userprofile =()=>{
     return(
         <>
-            <div class="min-h-screen p-6 bg-gray-100 flex items-center justify-center">
-  <div class="container max-w-screen-lg mx-auto">
+            <div className="min-h-screen p-6 bg-gray-100 flex items-center justify-center">
+  <div className="container max-w-screen-lg mx-auto">
     <div>
-      <div class="bg-white rounded shadow-lg p-4 px-4 md:p-8 mb-6">
-        <div class="grid gap-4 gap-y-2 text-sm grid-cols-1 lg:grid-cols-3">
-          <div class="text-gray-600">
-            <p class="font-medium text-lg">User Profile Details</p>
+      <div className="bg-white rounded shadow-lg p-4 px-4 md:p-8 mb-6">
+        <div className="grid gap-4 gap-y-2 text-sm grid-cols-1 lg:grid-cols-3">
+          <div className="text-gray-600">
+            <p className="font-medium text-lg">User Profile Details</p>
             <p>Please fill out all the fields.</p>
           </div>
 
-          <div class="lg:col-span-2">
-            <div class="grid gap-4 gap-y-2 text-sm grid-cols-1 md:grid-cols-5">
-              <div class="md:col-span-5">
+          <div className="lg:col-span-2">
+            <div className="grid gap-4 gap-y-2 text-sm grid-cols-1 md:grid-cols-5">
+              <div className="md:col-span-5">
                 <label for="full_name">Full Name</label>
-                <input type="text" name="full_name" id="full_name" class="h-10 border mt-1 rounded px-4 w-full bg-gray-50" value="" />
+                <input type="text" name="full_name" id="full_name" className="h-10 border mt-1 rounded px-4 w-full bg-gray-50 text-black" value="" />
               </div>
 
-              <div class="md:col-span-5">
+              <div className="md:col-span-5">
                 <label for="email">Email Address</label>
-                <input type="text" name="email" id="email" class="h-10 border mt-1 rounded px-4 w-full bg-gray-50" value="" placeholder="email@domain.com" />
+                <input type="text" name="email" id="email" className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" value="" placeholder="email@domain.com" />
               </div>
 
-              <div class="md:col-span-3">
+              <div className="md:col-span-3">
                 <label for="contact">Contact</label>
-                <input type="text" name="contact" id="contact" class="h-10 border mt-1 rounded px-4 w-full bg-gray-50" value="" placeholder="" />
+                <input type="text" name="contact" id="contact" className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" value="" placeholder="" />
               </div>
 
-              <div class="md:col-span-2">
+              <div className="md:col-span-2">
                 <label for="city">City</label>
-                <input type="text" name="city" id="city" class="h-10 border mt-1 rounded px-4 w-full bg-gray-50" value="" placeholder="" />
+                <input type="text" name="city" id="city" className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" value="" placeholder="" />
               </div>
 
-              <div class="md:col-span-2">
+              <div className="md:col-span-2">
                 <label for="password">Password</label>
-                <div class="h-10 bg-gray-50 flex border border-gray-200 rounded items-center mt-1">
-                  <input name="password" id="password" placeholder="Password" class="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent" value="" />
+                <div className="h-10 bg-gray-50 flex border border-gray-200 rounded items-center mt-1">
+                  <input name="password" id="password" placeholder="Password" className="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent" value="" />
                 </div>
               </div>
-              <div class="md:col-span-5 text-right">
-                <div class="inline-flex items-end">
-                  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Submit</button>
+              <div className="md:col-span-5 text-right">
+                <div className="inline-flex items-end">
+                  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Submit</button>
                 </div>
               </div>
 
