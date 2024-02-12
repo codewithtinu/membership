066c6b67c0acff4d9f4cfd8b8b4d6a9060117ee5
minal028 <codewithminal@gmail.com>
2024-02-12 14:30:39 +0530

    create admin Footer

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Register from './pages/Register';
 import Forgot from './pages/Forgot';
 import Userprofile from './pages/admin/UserProfile';
 import Navbar from './components/admin/Navabar';
+import AdminFooter from './components/admin/Footer';
 function App() {
   return (
     <>
@@ -17,6 +18,7 @@ function App() {
           <Route path="/forgot" element={<Forgot />} />
           <Route path='/userprofile' element={<Userprofile/>}/>
         </Routes>
+        <AdminFooter />
       </BrowserRouter>
     </>
   );
