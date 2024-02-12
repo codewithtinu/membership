7c45cea89db0bb95fa6d7d6cf7510179b99b9fbb
codewithpiyu <priyanka.nextsolution@gmail.com>
2024-02-12 17:07:48 +0530

    Change logo name

--- client/src/components/user/Header.js
+++ client/src/components/user/Header.js
@@ -1,7 +1,7 @@
 const Header = () =>{
     return(
         <>
-         <div class="text-red-600 md:text-blue-800 font-semibold text-2xl md:text-base uppercase ">logo</div>
+         <div class="text-red-600 md:text-blue-800 font-semibold text-2xl md:text-base uppercase ">Priyanka Kakde</div>
         </>
     )
 }
