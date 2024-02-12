ab65012ef7b1ba0b20546ccf55ed7a3da3ea98e9
codewithpiyu <priyanka.nextsolution@gmail.com>
2024-02-12 14:48:47 +0530

    resolved conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,8 @@ import Register from './pages/Register';
 import Forgot from './pages/Forgot';
 import Userprofile from './pages/admin/UserProfile';
 import Navbar from './components/admin/Navabar';
-<<<<<<< HEAD
 import Dashboard from './pages/admin/Dashboard';
-=======
 import UserNavbar from './components/user/Navbar';
->>>>>>> f14d2ea643c65b28589397995f3e3341fff28d40
 function App() {
   return (
     <>
