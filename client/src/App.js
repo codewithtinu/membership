1666eed51cc69b46b580a1d0f6b7187ff66b0181
KuldeepSonune45 <famlight@gmail.com>
2024-02-13 13:01:22 +0530

    Adding AdminFooter

--- client/src/App.js
+++ client/src/App.js
@@ -24,6 +24,7 @@ function App() {
           <Route path='/Dashboard' element={<Dashboard/>}/>
           <Route path='/UserList' element={<UserList/>}/>
         </Routes>
+        <AdminFooter/>
       </BrowserRouter> 
     </>
   );
